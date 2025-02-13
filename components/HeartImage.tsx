857ac2b6f7c178e781a225035fe54b34d43f3029
Pizza-NZ <ryanleo.nz.rsg@gmail.com>
2025-02-13 16:34:12 +1300

    Change img to Image for deployment issue

--- components/HeartImage.tsx
+++ components/HeartImage.tsx
@@ -1,16 +1,15 @@
 import styles from "@/app/mystyle.module.css";
+import Image from "next/image";
 
 const HeartImage = ({ width = 100, height = 100, ...props }) => {
     return (
       <div>
-        <img
-          src="/heart.svg"
+        <Image src="/heart.svg"
           alt="heart"
           width={width}
           height={height}
           className={`${styles.heartImage} mx-auto my-auto`}
-          {...props}
-        />
+          {...props}/>
       </div>
     );
   };
